c409c5013ef21558a6ca5658caaecac3be259b2a
kholiavkoi <kholiavkoi@gmail.com>
2025-01-14 21:51:22 +0100

    added microservice

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { FlowersModule } from './flowers/flowers.module';
 import { LoggerMiddleware } from './conception/middleware';
 import { ConfigModule } from '@nestjs/config';
+import { MicroserviceModule } from './microservice/microservice.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), FlowersModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), FlowersModule, MicroserviceModule],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  const app =
+    await NestFactory.createMicroservice<MicroserviceOptions>(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
       whitelist: true,
     }),
   );
-  await app.listen(process.env.PORT ?? 4200);
+  await app.listen();
 }
 bootstrap();
